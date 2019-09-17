d0efb7bad1a46335dd31c206bd9177df3e06b896
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-17 18:21:41 +0200

    add nav

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+//smooth scroll
+
+$(".navBtn").on("click", function(e) {
+  const goToSection = `.${e.target.dataset.target}`;
+  $("body, html").animate({
+    scrollTop: $(goToSection).offset().top - (window.innerWidth < 900 ? 0 : 100)
+  });
+});
+
+//handle nav
+
+const nav = document.querySelector(".nav");
+const hamburger = document.querySelector(".hamburger");
+const navBtns = document.querySelectorAll(".navBtn");
+const main = document.querySelector("main");
+
+const toggleNav = () => {
+  nav.classList.toggle("active");
+};
+
+hamburger.addEventListener("click", toggleNav);
+navBtns.forEach(btn => btn.addEventListener("click", toggleNav));
+main.addEventListener("click", () => {
+  if (nav.classList.value.includes("active")) {
+    nav.classList.remove("active");
+  }
+});
